eae3a9e8b47082c5c05ed208d48be4b6de0015ea
emeraldgoose <emeraldgoose@gmail.com>
2021-09-19 05:48:26 +0000

    formatting autopep

--- Transformer/SelfAttention.py
+++ Transformer/SelfAttention.py
@@ -21,35 +21,43 @@ class SelfAttention(nn.Module):
         key_weight: linear transform weights
         value_weight: linear transform weights
     """
+
     def __init__(self, input_dimension, key_dimension, value_dimension):
         super().__init__()
         self.key_dimension = key_dimension
-        self.query_weight = nn.Linear(in_features=input_dimension, out_features=key_dimension, bias=False)
-        self.key_weight = nn.Linear(in_features=input_dimension, out_features=key_dimension, bias=False)
-        self.value_weight = nn.Linear(in_features=input_dimension, out_features=value_dimension, bias=False)
+        self.query_weight = nn.Linear(
+            in_features=input_dimension, out_features=key_dimension, bias=False)
+        self.key_weight = nn.Linear(
+            in_features=input_dimension, out_features=key_dimension, bias=False)
+        self.value_weight = nn.Linear(
+            in_features=input_dimension, out_features=value_dimension, bias=False)
 
     def forward(self, inputs):
         querys = self.query_weight(inputs)
         keys = self.key_weight(inputs)
         values = self.value_weight(inputs)
-        
-        scaled = (querys @ keys.T) / np.math.sqrt(self.key_dimension) # (number_of_inputs, number_of_inputs)
-        value_weight = F.softmax(scaled, dim=1) # row-wised softmax
+
+        # (number_of_inputs, number_of_inputs)
+        scaled = (querys @ keys.T) / np.math.sqrt(self.key_dimension)
+        value_weight = F.softmax(scaled, dim=1)  # row-wised softmax
         # 여기서부터 참고함
-        weighted = values[:,None] * value_weight.T[:,:,None]
+        weighted = values[:, None] * value_weight.T[:, :, None]
         return weighted.sum(dim=0)
 
 
 def main():
     number_of_input_vector = 3
     input_dimension = 3
-    input_vectors = np.array([np.random.randn(input_dimension) for _ in range(number_of_input_vector)])
+    input_vectors = np.array([np.random.randn(input_dimension)
+                             for _ in range(number_of_input_vector)])
     input_vectors = torch.tensor(input_vectors, dtype=torch.float)
 
-    model = SelfAttention(input_dimension=input_dimension, key_dimension=3, value_dimension=3)
+    model = SelfAttention(input_dimension=input_dimension,
+                          key_dimension=3, value_dimension=3)
 
     output = model(input_vectors)
     print(output)
+
 
 if __name__ == "__main__":
     main()
